0c054d5b887d88460547956f71c449f4566309cd
rabiulhossen <rhsimon69@gmail.com>
2022-07-16 23:04:44 +0600

    added

---
+++ data/data.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+
+
+const baseUrl
+// headers: {
+//      'X-RapidAPI-Key': '9640afbc2amsh0434d678b099846p1b038djsn5a88ae357cfa',
+//      'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+//    }
--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,51 @@ import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
 import Link from "next/link";
 
-const Banner = ({ purpose }) => {
-  <Flex flexWrap="wrap" justifyContent="center" m="10">
-   
-  </Flex>;
+function Fanner ({
+  purpose,
+  title1,
+  title2,
+  desc1,
+  desc2,
+  buttonText,
+  imageUrl,
+  linkName,
+})  {
+  return(
+    <Flex flexWrap="wrap" m="10">
+    {/* <Image src={imageUrl} width={500} height={400} alt="banner" /> */}
+    <Box p="5">
+      <Text color="gray.500" fontSize="sm" fontWeight="medium">
+        {purpose}
+      </Text>
+      <Text color="gray.500" fontSize="3xl" fontWeight="bold">
+        {title1} <br /> {title2}
+      </Text>
+      <Text color="gray.700" paddingTop="3" fontSize="lg">
+        {desc1} <br /> {desc2}
+      </Text>
+      <Button fontSize="xl" bg="blackAlpha.400" color="white">
+        {/* <Link href={linkName}>{buttonText}</Link> */}
+      </Button>
+    </Box>
+    
+  </Flex>
+  )
 };
 export default function Home() {
   return (
     <div>
-      <h1 >
+      <h1 color="gray.500">
         hello buyer
-        <Banner  purpose={"for sale"}/>
+        <Fanner
+          purpose="for sale"
+          title1="sale"
+          desc1="explore apartment"
+          buttonText="explore new"
+          title2="everyone"
+        />
+        kjkj
       </h1>
-      
     </div>
   );
 }
